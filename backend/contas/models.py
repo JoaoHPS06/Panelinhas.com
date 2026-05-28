a5419330c3cb63685a501344c996142e70ce0396
Gabriel Vilas Novas Sousa <gabriel.novas@aluno.ufop.edu.br>
2026-05-28 19:56:04 -0300

    Criação dos modelos de usuário e usuário admin.

--- backend/contas/models.py
+++ backend/contas/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class usuarioCustomizado(AbstractUser):
+    # O AbstractUser já traz username, first_name, last_name, email e password.
+    # Complementaremos com os campos email, eh_dono_loja e telefone.
+    
+    email = models.EmailField(unique=True) # Exigimos que o email seja único
+    eh_dono_loja = models.BooleanField(
+        default=False, 
+        help_text="Marcar se este usuário é um estabelecimento comercial."
+    )
+    telefone = models.CharField(max_length=15, blank=True, null=True)
+
+    # Usar o email como campo de login principal 
+    USERNAME_FIELD = 'email'
+    
+    # Campos obrigatórios ao criar um superusuário no terminal
+    REQUIRED_FIELDS = ['username', 'first_name']
+
+    def __str__(self):
+        return self.email
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# App 'contas', modelo 'usuarioCustomizado'
+AUTH_USER_MODEL = 'contas.usuarioCustomizado'
