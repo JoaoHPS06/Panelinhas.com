ba433229da0da107db5f96c8b0c0d5732bded48b
João Henrique Pedrosa de Souza <joao.hps@aluno.ufop.edu.br>
2026-06-24 10:00:10 -0300

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { Link, useLocation } from "react-router-dom";
 
 // 1. IMPORTANDO A IMAGEM: 
-import logoPanelinhas from "../assets/logo_laranja.svg"; 
+import logoPanelinhas from "../assets/logo.png"; 
 
 // 2. A "STRUCT" DO TYPESCRIPT:
 interface ItemDoMenu {
